39a7a975680fb1ffaa18d09882ef9f4b60cff8a2
Hüseyin Şimşek <huseyin.simsek@boun.edu.tr>
2023-03-27 12:07:43 +0300

    Add project files.

---
+++ Controllers/PublisherController.cs
@@ -0,0 +1,29 @@
+﻿using BookStoreCet.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreCet.Controllers
+{
+    public class PublisherController : Controller
+    {
+        ApplicationDbContext dbContext;
+        public PublisherController(ApplicationDbContext dbContext)
+        {
+            this.dbContext= dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Publisher> publishers = dbContext.Publishers.ToList();
+            return View(publishers);
+        }
+
+        public IActionResult Details (int id)
+        {
+            Publisher publisher= dbContext.Publishers.Find(id);
+            if(publisher == null)
+            {
+                return NotFound();
+            }
+            return View(publisher);
+        }
+    }
+}
---
+++ Data/Book.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreCet.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name  { get; set; }
+        public Decimal Price { get; set; }
+
+        public int PageCount { get; set; }
+
+        public DateTime PublishDate { get; set; }   
+
+        public int? PublisherId { get; set; }
+
+        public virtual Publisher Publisher { get; set; }
+
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Book> Books { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public virtual List<Book> Books { get; set; }
+    }
+}
